f7fb390520bce8cdd0e41dc367f9d71eedb38f2b
Eric Amundson <eric@ivycat.com>
2015-07-02 19:53:51 -0700

    code cleanup

--- lib/IvyCatTestimonialsPosts.php
+++ lib/IvyCatTestimonialsPosts.php
@@ -3,8 +3,9 @@
  *	Page Posts Class, main workhorse for the ic_add_testimonials shortcode.
  */
 
-if ( !function_exists( 'add_action' ) )
+if ( ! function_exists( 'add_action' ) ) {
 	wp_die( __( 'You are trying to access this file in a manner not allowed.', 'ivycat-ajax-testimonials' ), __( 'Direct Access Forbidden', 'ivycat-ajax-testimonials' ), array( 'response' => '403' ) );
+}
 
 class ICTestimonialPosts {
 
@@ -14,7 +15,7 @@ class ICTestimonialPosts {
 		'orderby'		=> 'date',
 		'order'			=> 'DESC',
 		'paginate'		=> false,
-		'template'		=> false
+		'template'		=> false,
 	); // set defaults for wp_parse_args
 
 	public function __construct( $atts ) {
@@ -27,7 +28,7 @@ class ICTestimonialPosts {
 	 *	@return string output of template file
 	 */
 	public function output_testimonials() {
-		if ( !$this->args )
+		if ( ! $this->args )
 			return '';
 		$page_testimonials = apply_filters( 'testimonials_in_page_results', new WP_Query( $this->args ) ); // New WP_Query object
 		$output = '';
@@ -55,7 +56,6 @@ class ICTestimonialPosts {
 		$total_pages = $posts->max_num_pages;
 		$per_page = $posts->query_vars['posts_per_page'];
 		$curr_page = ( isset( $posts->query_vars['paged'] ) && $posts->query_vars['paged'] > 0  ) ? $posts->query_vars['paged'] : 1;
-		//echo '<pre>' . print_r( $posts, true ) . '</pre>';
 		$prev = ( $curr_page && $curr_page > 1 ) ? '<li><a href="'.$page_url.'?page='. ( $curr_page-1 ).'">Previous</a></li>' : '';
 		$next = ( $curr_page && $curr_page < $total_pages ) ? '<li><a href="'.$page_url.'?page='. ( $curr_page+1 ).'">Next</a></li>' : '';
 		return '<ul>' . $prev . $next . '</ul>';
@@ -72,25 +72,26 @@ class ICTestimonialPosts {
 		// parse the arguments using the defaults
 		$this->args = wp_parse_args( $atts, $this->args );
 
-
 		// Use a specified template
-		if ( isset( $atts['template'] ) )
+		if ( isset( $atts['template'] ) ) {
 			$this->args['template'] = $atts['template'];
+		}
 
 		// show number of posts (default is 10, showposts or posts_per_page are both valid, only one is needed)
-		if ( isset( $atts['showposts'] ) )
+		if ( isset( $atts['showposts'] ) ) {
 			$this->args[ 'posts_per_page' ] = $atts['showposts'];
+		}
 
 		// handle pagination (for code, template pagination is in the template)
 		if ( isset( $wp_query->query_vars['page'] ) &&  $wp_query->query_vars['page'] > 1 ) {
 			$this->args['paged'] = $wp_query->query_vars['page'];
 		}
-	    if ( $atts['group'] !== false ) {
+	    if ( false !== $atts['group'] ) {
 			$this->args['tax_query'] = array(
 					array(
 					'taxonomy' => 'testimonial-group',
 					'field' => is_numeric( $atts['group'] ) ? 'id' : 'slug',
-					'terms' => $atts['group']
+					'terms' => $atts['group'],
 				)
 			);
 		}
@@ -114,7 +115,7 @@ class ICTestimonialPosts {
 	 *
 	 *	@return string results of the output
 	 */
-   protected function add_template_part( $ic_testimonials, $singles=false ) {
+   protected function add_template_part( $ic_testimonials, $singles = false ) {
 		if ( $singles ) {
 			setup_postdata( $ic_testimonials );
 		} else {
